fa2d64b23f8cfdc98662e61c0b06b796a5869cc0
Karol <karol.vogelgezang@gmail.com>
2019-10-16 23:13:06 +0200

    teraz masz robic lekcje nr 6

--- vuejs-playlist/app.js
+++ vuejs-playlist/app.js
@@ -4,11 +4,30 @@ new Vue({
         name: 'Shaun',
         job: 'Ninja',
         website: 'https://www.google.pl',
-        websiteTag: "<a href='https://www.google.pl'>tag </a>"
+        websiteTag: "<a href='https://www.google.pl'>tag </a>",
+        age: 28,
+        x: 0,
+        y: 0
     },
     methods: {
         greet: function(time){
             return 'Good ' + time + ', ' + this.name;
+        },
+        add: function() {
+            this.age++
+        },
+        subtract: function() {
+            this.age--;
+        },
+        add2: function(inc) {
+            this.age += inc;
+        },
+        subtract2: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 });
