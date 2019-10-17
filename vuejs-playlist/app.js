74adac54730ff926ac189538051b7f2c54fd4815
Vogelgezang <Karol.Vogelgezang.ctr@sabre.com>
2019-10-17 15:10:26 +0200

    Teraz masz robić lekcję nr 10

--- vuejs-playlist/app.js
+++ vuejs-playlist/app.js
@@ -4,10 +4,15 @@ new Vue({
         name: 'Shaun',
         job: 'Ninja',
         website: 'https://www.google.pl',
-        websiteTag: "<a href='https://www.google.pl'>tag </a>",
+        websiteTag: "<a href='https://www.google.pl'>tag</a>",
         age: 28,
         x: 0,
-        y: 0
+        y: 0,
+        ourText: "",
+        a: 0,
+        b: 0,
+
+        // do przykładu do pracy
     },
     methods: {
         greet: function(time){
@@ -28,6 +33,41 @@ new Vue({
         updateXY: function(e) {
             this.x = e.offsetX;
             this.y = e.offsetY;
+        },
+        click: function () {
+            alert("You clicked me");
+        },
+        logName: function () {
+            console.log('you entered your name')
+        },
+        logAge: function () {
+            console.log('you entered your age')
+        },
+        // to poniżej bedzie w computed
+        // addToA: function () {
+        //     console.log('addToA');
+        //     return this.a + this.age
+        // },
+        // addToB: function () {
+        //     console.log('addToB');
+        //     return this.b + this.age
+        // },
+
+
+
+        // Przykład do pracy
+        show: function() {
+
         }
+    },
+    computed: {
+        addToA: function () {
+            console.log('addToA');
+            return this.a + this.age
+        },
+        addToB: function () {
+            console.log('addToB');
+            return this.b + this.age
+        },
     }
 });
