7a65b5d7cd2e0de55ddf05c5983a3a938cbb7954
Karol <karol.vogelgezang@gmail.com>
2019-10-27 22:31:24 +0100

    Teraz robisz lekcje nr 19

---
+++ vue-cli/vuejs-playlist/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        {{ title }}
+        <p>{{ greeting() }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+// Pamiętaj, że tutaj ten zapis któ©ego używasz i ten poniżśzy to to samo
+// to tylko nowy ES6 feature
+// Stary zapis (działą taks amo)
+//   data: function() {
+//       return {
+//           message: "Wiadomość od Karola"
+//       }
+//   },
+
+  data() {
+      return {
+          title: "Wiadomość od Karola"
+      }
+  },
+    methods: {
+      greeting: function() {
+        return 'Hej Karol'
+      },
+
+      // Nowy zapis (ES6)
+      // greeting() {
+      //   return 'Hej Karol'
+      // }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
